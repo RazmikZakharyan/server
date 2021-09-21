d806378fa7e8e46be0fe1f87fb5409f3318994b2
Razmik <48383038+RazmikZakharyan@users.noreply.github.com>
2021-09-21 18:01:49 +0400

    Add files via upload

---
+++ server.py
@@ -0,0 +1,59 @@
+import socket
+import asyncio
+
+
+class Server:
+    def __init__(self):
+        self.socket = socket.socket(
+            socket.AF_INET,
+            socket.SOCK_STREAM,
+        )
+        self.main_loop = asyncio.get_event_loop()
+
+        self.users = []
+
+    def set_up(self):
+        self.socket.bind(
+            ("127.0.0.1", 8000)
+        )
+
+        self.socket.listen(5)
+        self.socket.setblocking(False)
+        print("Server is listening")
+
+    async def send_data(self, data, user_):
+        for user in self.users:
+            await self.main_loop.sock_sendall(user,
+                                              f'|{user_.getsockname()[0]}|'
+                                              f'{data.decode()}'.encode())
+
+    async def listen_socket(self, listened_socket: socket.socket = None):
+        while True:
+            try:
+                data = await self.main_loop.sock_recv(listened_socket, 2024)
+                await self.send_data(data, listened_socket)
+            except ConnectionResetError:
+                self.users.remove(listened_socket)
+                print(f'User disconnected')
+                return
+
+    async def accept_sockets(self):
+        while True:
+            user_socket, address = await self.main_loop.sock_accept(
+                self.socket)
+            print(f"User <{address[0]}> connected!")
+
+            self.users.append(user_socket)
+            self.main_loop.create_task(self.listen_socket(user_socket))
+
+    async def main(self):
+        await self.main_loop.create_task(self.accept_sockets())
+
+    def start(self):
+        self.main_loop.run_until_complete(self.main())
+
+
+if __name__ == '__main__':
+    server = Server()
+    server.set_up()
+    server.start()
